3dd72840f9839bc664006185fedee2dfe0341e7d
jjoon <113758652+joonho4@users.noreply.github.com>
2023-11-21 00:55:29 +0900

    Add files via upload

---
+++ SpringLearn1/src/main/java/com/example/springlearn1/SpringLearn1Application.java
@@ -0,0 +1,16 @@
+package com.example.springlearn1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class SpringLearn1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearn1Application.class, args);
+        
+    }
+
+}
