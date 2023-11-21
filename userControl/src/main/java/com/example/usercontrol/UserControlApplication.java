6f7970b44cc8ee290107375c64ecb74877468736
jjoon <113758652+joonho4@users.noreply.github.com>
2023-11-21 19:33:29 +0900

    Add files via upload

---
+++ userControl/src/main/java/com/example/usercontrol/UserControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.usercontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserControlApplication.class, args);
+    }
+
+}
---
+++ userControl/src/test/java/com/example/usercontrol/UserControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usercontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
